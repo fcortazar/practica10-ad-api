57b5dcc4669f3176bb068e4b1a019c16926f3ff0
Fran Cortázar <fcd0005@alu.medac.es>
2023-12-18 12:45:19 +0100

    Inicio

---
+++ src/test/java/com/DI/practica10/Practica10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DI.practica10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
