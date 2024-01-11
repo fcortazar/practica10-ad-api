34b7dd2db2db7955cb740070a92a4e00329ce9f4
Fran Cortázar <fcd0005@alu.medac.es>
2024-01-11 14:05:51 +0100

    Entrada de bbdd

---
+++ src/main/java/com/DI/practica10/GetAlumnos.java
@@ -0,0 +1,11 @@
+package com.DI.practica10;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alumnos")
+public class GetAlumnos {
+
+}
--- src/main/java/com/DI/practica10/Practica10Application.java
+++ src/main/java/com/DI/practica10/Practica10Application.java
@@ -2,12 +2,12 @@ package com.DI.practica10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Practica10Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Practica10Application.class, args);
 	}
-
 }
