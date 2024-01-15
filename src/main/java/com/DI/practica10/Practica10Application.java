05c9db15189fea3d3baa7976ed07b4c86b77b9bd
diego <diegorosillo15@gmail.com>
2024-01-15 20:28:08 +0100

    holaa

--- src/main/java/com/DI/practica10/Practica10Application.java
+++ src/main/java/com/DI/practica10/Practica10Application.java
@@ -3,11 +3,12 @@ package com.DI.practica10;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//holaa
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Practica10Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Practica10Application.class, args);
 	}
+
 }
