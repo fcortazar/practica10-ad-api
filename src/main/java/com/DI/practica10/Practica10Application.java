2247751998ffd1e96c4a777d4a489f655fd93440
diego <diegorosillo15@gmail.com>
2024-01-15 19:41:03 +0100

    hola

--- src/main/java/com/DI/practica10/Practica10Application.java
+++ src/main/java/com/DI/practica10/Practica10Application.java
@@ -3,7 +3,7 @@ package com.DI.practica10;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//hola
+//holaa
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Practica10Application {
 
